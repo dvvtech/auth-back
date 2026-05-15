3485a0d81a360425e4ff94d834aac226692dc5bc
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-05-15 18:43:12 +0300

    add GoogleAuthorizeController

---
+++ src/Auth.Api/Controllers/GoogleAuthorizeController.cs
@@ -0,0 +1,53 @@
+﻿using Auth.Api.BLL.Services;
+using Auth.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.Api.Controllers
+{
+    [Route("google-auth")]
+    [ApiController]
+    public class GoogleAuthorizeController : ControllerBase
+    {
+        private readonly GoogleAuthService _authService;
+        private readonly ILogger<GoogleAuthorizeController> _logger;
+
+        public GoogleAuthorizeController(
+            GoogleAuthService googleAuthService,
+            ILogger<GoogleAuthorizeController> logger)
+        {
+            _authService = googleAuthService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Authorize()
+        {            
+            var authUrl = _authService.GenerateAuthUrl();
+            return Ok(authUrl);
+        }
+
+        [HttpGet("callback")]
+        public async Task<IActionResult> Callback(string code)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(code))
+                {
+                    return BadRequest("Google auth: Code is required");
+                }
+
+                TokenResponse tokenResponse = await _authService.HandleCallback(code);
+
+                //Перенаправляем пользователя на фронтенд
+                return Redirect($"https://somedomain.com?" +
+                                $"accessToken={Uri.EscapeDataString(tokenResponse.AccessToken)}&" +
+                                $"refreshToken={Uri.EscapeDataString(tokenResponse.RefreshToken)}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Google auth exception: {ex.Message}");
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
